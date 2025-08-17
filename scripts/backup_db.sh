f722e1266210fc0a3e7a4b0c1972787f93ec6d5a
Nik <nshamanov@mail.ru>
2025-08-18 00:57:06 +0300

    fix: replace docker-compose with docker compose

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -5,7 +5,8 @@ BACKUP_FILE="db_dump_$(date +%Y-%m-%d).sql"
 S3_PATH="s3://$S3_BUCKET_NAME/backups/$BACKUP_FILE"
 
 # Дамп БД и загрузка в S3
-docker-compose exec -T db pg_dump -U $PGUSER -d $PGDATABASE > $BACKUP_FILE
+# docker-compose exec -T db pg_dump -U $PGUSER -d $PGDATABASE > $BACKUP_FILE - это не рабочий код - дефис
+docker compose exec -T db pg_dump -U $PGUSER -d $PGDATABASE > $BACKUP_FILE
 aws --endpoint-url=$S3_ENDPOINT s3 cp $BACKUP_FILE $S3_PATH
 aws --endpoint-url=$S3_ENDPOINT s3 cp $S3_PATH "s3://$S3_BUCKET_NAME/backups/db_dump_latest.sql"
 rm $BACKUP_FILE
