8dbd968331e1371d3b9eae3b1936883a25706620
Nik <nshamanov@mail.ru>
2025-08-20 01:38:49 +0300

    fixw/ossl: disable SSL verification for backups

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -1,7 +1,25 @@
+# #!/bin/bash
+# source ~/.bashrc  # Теперь загружаем .bashrc
+# source .env
+
+# export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY
+# export AWS_SECRET_ACCESS_KEY=$S3_SECRET_KEY
+
+# BACKUP_FILE="db_dump_$(date +%Y-%m-%d).sql"
+# S3_PATH="s3://$S3_BUCKET_NAME/backups/$BACKUP_FILE"
+
+# docker compose exec -T db pg_dump -U $PGUSER -d $PGDATABASE > $BACKUP_FILE
+
+# aws --endpoint-url=$S3_ENDPOINT s3 cp $BACKUP_FILE $S3_PATH
+# aws --endpoint-url=$S3_ENDPOINT s3 cp $BACKUP_FILE "s3://$S3_BUCKET_NAME/backups/db_dump_latest.sql"
+
+# rm $BACKUP_FILE
+# echo "Backup completed: $S3_PATH"
+
 #!/bin/bash
-source ~/.bashrc  # Теперь загружаем .bashrc
 source .env
 
+# Настройка без SSL-проверки
 export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$S3_SECRET_KEY
 
@@ -10,8 +28,8 @@ S3_PATH="s3://$S3_BUCKET_NAME/backups/$BACKUP_FILE"
 
 docker compose exec -T db pg_dump -U $PGUSER -d $PGDATABASE > $BACKUP_FILE
 
-aws --endpoint-url=$S3_ENDPOINT s3 cp $BACKUP_FILE $S3_PATH
-aws --endpoint-url=$S3_ENDPOINT s3 cp $BACKUP_FILE "s3://$S3_BUCKET_NAME/backups/db_dump_latest.sql"
+aws --no-verify-ssl --endpoint-url=$S3_ENDPOINT s3 cp $BACKUP_FILE $S3_PATH
+aws --no-verify-ssl --endpoint-url=$S3_ENDPOINT s3 cp $BACKUP_FILE "s3://$S3_BUCKET_NAME/backups/db_dump_latest.sql"
 
 rm $BACKUP_FILE
 echo "Backup completed: $S3_PATH"
